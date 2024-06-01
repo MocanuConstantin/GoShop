bcb7c0185f4334c471743b08d9f1f3d4e9a32fad
Mocanu Constantin <mocanu.constantin101@gmail.com>
2024-06-01 13:40:58 +0300

    Add EntityClass

---
+++ GoShopAPI/GoShop.Domain/Entities/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoShop.Domain.Entities;
+
+public class Entity
+{
+}
