0e014e84f5736e34ab0e81b1171a58fbafa1ddca
Mocanu Constantin <mocanu.constantin101@gmail.com>
2024-06-29 16:18:24 +0300

    Add FiltersModels

---
+++ GoShopAPI/GoShop.Domain/Models/MobilePhoneFiltersModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoShop.Domain.Models;
+
+public class MobilePhoneFiltersModel
+{
+    public int? Count { get; set; }
+    public int? Offset { get; set; }
+    public string? SortBy { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public decimal Price { get; set; } = 0;
+}
