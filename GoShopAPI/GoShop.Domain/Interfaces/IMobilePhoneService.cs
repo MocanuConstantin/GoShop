5f97399af1e7f0499f7bf111a9c8ea614a9a6b77
Mocanu Constantin <mocanu.constantin101@gmail.com>
2024-07-04 00:25:56 +0300

    Add service-interfaces

---
+++ GoShopAPI/GoShop.Domain/Interfaces/IMobilePhoneService.cs
@@ -0,0 +1,16 @@
+﻿using GoShop.Domain.Entities;
+using GoShop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoShop.Domain.Interfaces;
+
+public interface IMobilePhoneService
+{
+    Task<List<MobilePhoneEntity>> GetAllAsync(MobilePhoneFiltersModel model, CancellationToken cancellationToken);
+
+    Task<int> GetCountByFiltersAsync(MobilePhoneFiltersModel model, CancellationToken cancellationToken);
+}
---
+++ GoShopAPI/GoShop.Domain/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using GoShop.Domain.Entities;
+using GoShop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoShop.Domain.Interfaces;
+
+public interface IUserService
+{
+    Task<List<UserEntity>> GetAllAsync(UserFiltersModel model, CancellationToken cancellationToken);
+
+    Task<int> GetCountByFiltersAsync(UserFiltersModel model, CancellationToken cancellationToken);
+}
