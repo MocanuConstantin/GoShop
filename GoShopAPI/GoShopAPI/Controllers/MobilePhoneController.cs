f5f4ae84bc9780add7f5589729978d2b618f94b0
Mocanu Constantin <mocanu.constantin101@gmail.com>
2024-07-09 16:19:37 +0300

    Add Controllers and GetMethods endpoints

---
+++ GoShopAPI/GoShopAPI/Controllers/MobilePhoneController.cs
@@ -0,0 +1,36 @@
+﻿using GoShop.Domain.Entities;
+using GoShop.Domain.Interfaces;
+using GoShop.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MobilePhoneController : ControllerBase
+    {
+        private readonly IMobilePhoneService _mobilePhoneService;
+
+        public MobilePhoneController(IMobilePhoneService mobilePhoneService)
+        {
+            _mobilePhoneService = mobilePhoneService;
+        }
+
+        // GET: api/MobilePhones
+        [HttpGet]
+        public async Task<ActionResult<List<MobilePhoneFiltersModel>>> GetAll([FromQuery] MobilePhoneFiltersModel model, CancellationToken cancellationToken)
+        {
+            var result = await _mobilePhoneService.GetAllAsync(model, cancellationToken);
+            return Ok(result);
+        }
+
+        // GET: api/MobilePhones/Count
+        [HttpGet("count")]
+        public async Task<ActionResult<int>> GetCountByFilters([FromQuery] MobilePhoneFiltersModel model, CancellationToken cancellationToken)
+        {
+            var count = await _mobilePhoneService.GetCountByFiltersAsync(model, cancellationToken);
+            return Ok(count);
+        }
+    }
+}
--- GoShopAPI/GoShopAPI/Program.cs
+++ GoShopAPI/GoShopAPI/Program.cs
@@ -5,6 +5,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
 using GoShop.Domain;
 using GoShop.Data;
+using GoShop.Core.Services;
+using GoShop.Data.Repositories;
+using GoShop.Domain.Interfaces;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +15,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IMobilePhoneService, MobilePhoneService>();
+builder.Services.AddScoped<IMobilePhoneRepository, MobilePhoneRepository>();
+builder.Services.AddScoped<IMobilePhoneHardwareService, MobilePhoneHardwareService>();
+builder.Services.AddScoped<IMobilePhoneHardwareRepository, MobilePhoneHardwareRepository>();
+builder.Services.AddScoped<IMobilePhoneSoftwareService, MobilePhoneSoftwareService>();
+builder.Services.AddScoped<IMobilePhoneSoftwareRepository, MobilePhoneSoftwareRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 builder.Services.AddDbContext<GoShopDbContext>(options =>
        options.UseSqlServer(builder.Configuration.GetConnectionString("GoShopDatabase")));
